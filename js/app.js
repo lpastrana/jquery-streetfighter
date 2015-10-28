d10fb4a9d3fe144a3a7b103aee57b715a18ebe70
Luis Pastrana <lpastrana2@hotmail.com>
2015-10-27 21:32:38 -0400

    updating files

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
   $('.ryu').mouseenter(function(){
   	$('.ryu-still').hide();
   	$('.ryu-ready').show();
+    $('.ryu-cool').hide();
    })
   .mouseleave(function(){
   	$('.ryu-ready').hide();
   	$('.ryu-still').show();
+    $('.ryu-cool').hide();
   })
   .mousedown(function(){
   	playHadouken();
@@ -23,7 +25,25 @@ $(document).ready(function(){
   .mouseup(function(){
     $('.ryu-throwing').hide();    
     $('.ryu-ready').show();
+    $('.ryu-cool').hide();
   });
+
+  $(window).keydown(function(e) {
+    if (e.keyCode === 88) {
+    $('.ryu-cool').show();
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+      } 
+  });
+
+  $(window).keyup(function(e) {
+    if (e.keyCode === 88) {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+      } 
+  });
+
 });
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
